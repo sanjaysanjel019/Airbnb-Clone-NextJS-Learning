2a213c1ccd82b5b7056cb2a4b0152139777e1f3b
Sanjay Sanjel <sanjay.sanjel018@gmail.com>
2023-04-30 23:52:51 +0545

    added register modal

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -13,7 +13,7 @@ interface ModalProps {
   actionLabel: string;
   disabled?: boolean;
   secondaryAction?: () => void;
-  secondaryLabel: string;
+  secondaryActionLabel: string;
 }
 
 const Modal: React.FC<ModalProps> = ({
@@ -26,7 +26,7 @@ const Modal: React.FC<ModalProps> = ({
   actionLabel,
   disabled,
   secondaryAction,
-  secondaryLabel,
+  secondaryActionLabel,
 }) => {
   const [showModal, setShowModal] = useState(isOpen);
 
@@ -119,7 +119,22 @@ const Modal: React.FC<ModalProps> = ({
             {/* Foooter */}
             <div className="flex flex-col gap-2 p-6">
                 <div className="flex flex-row items-center gap-4 w-full">
-                    <Button label={"CLick Here"} onClick={()=>{}}/>
+
+                
+                {secondaryAction && secondaryActionLabel && (
+                     <Button 
+                     outline
+                         disabled={disabled}
+                         label={secondaryActionLabel}
+                         onClick={handleSecondaryAction}/>
+                )}
+               
+
+
+                    <Button 
+                    disabled={disabled}
+                    label={actionLabel}
+                    onClick={handleSubmit}/>
                 </div>
             </div>
             </div>
